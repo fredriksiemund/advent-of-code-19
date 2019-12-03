81a6c10819fffcbc75016d99b19c9e9a91892664
Fredrik Siemund <fredrik.siemund@gmail.com>
2019-12-03 17:15:40 +0100

    2 part 1

--- 2/main.py
+++ 2/main.py
@@ -4,19 +4,22 @@ import math
 with open("input.txt", "r") as i:
     input = i.readline()
     input = input.split(',')
-    for i in range(0, len(input)):
-        if input[i] is '1':
-            address1 = int(input[i+1])
-            address2 = int(input[i+2])
-            address3 = int(input[i+3])
-            input[address3] = str(int(input[address1]) + int(input[address2]))
-            i += 3
-        elif input[i] is '2':
-            address1 = int(input[i+1])
-            address2 = int(input[i+2])
-            address3 = int(input[i+3])
-            input[address3] = str(int(input[address1]) * int(input[address2]))
-            i += 3
-        elif input[i] is '99':
-            break
-    print(input)
+    i = 0
+    while i < len(input):
+      input_nbr = int(input[i])
+      if input_nbr is 1:
+        address1 = int(input[i+1])
+        address2 = int(input[i+2])
+        address3 = int(input[i+3])
+        input[address3] = str(int(input[address1]) + int(input[address2]))
+        i += 3
+      elif input_nbr is 2:
+        address1 = int(input[i+1])
+        address2 = int(input[i+2])
+        address3 = int(input[i+3])
+        input[address3] = str(int(input[address1]) * int(input[address2]))
+        i += 3
+      elif input_nbr is 99:
+        break
+      i += 1
+    print(input[0])
