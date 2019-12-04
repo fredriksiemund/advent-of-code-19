cc98cd3d1cfd28d222b8400989d0d680a5416e88
Fredrik Siemund <fredrik.siemund@gmail.com>
2019-12-04 11:31:33 +0100

    refactor

--- 2/main.py
+++ 2/main.py
@@ -4,22 +4,20 @@ import math
 with open("input.txt", "r") as i:
     input = i.readline()
     input = input.split(',')
+
     i = 0
     while i < len(input):
       input_nbr = int(input[i])
-      if input_nbr is 1:
-        address1 = int(input[i+1])
-        address2 = int(input[i+2])
-        address3 = int(input[i+3])
-        input[address3] = str(int(input[address1]) + int(input[address2]))
-        i += 3
-      elif input_nbr is 2:
-        address1 = int(input[i+1])
-        address2 = int(input[i+2])
-        address3 = int(input[i+3])
-        input[address3] = str(int(input[address1]) * int(input[address2]))
-        i += 3
-      elif input_nbr is 99:
+      if input_nbr is 99:
         break
-      i += 1
+
+      address1 = int(input[i+1])
+      address2 = int(input[i+2])
+      address3 = int(input[i+3])
+      if input_nbr is 1:
+        input[address3] = str(int(input[address1]) + int(input[address2]))
+      elif input_nbr is 2:
+        input[address3] = str(int(input[address1]) * int(input[address2]))
+      i += 4
+      
     print(input[0])
