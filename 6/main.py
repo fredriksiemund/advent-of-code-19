c220ff8fafcef980d40a64f78423fc235248297a
fredriksiemund <fredrik.siemund@gmail.com>
2019-12-06 15:29:45 +0100

    6 part 1 done

--- 6/main.py
+++ 6/main.py
@@ -2,22 +2,28 @@ import math
 import sys
 from collections import defaultdict
 
+grand_total = 0
+
+def count_orbits(solar_system, current_planet, orbit_total):
+  global grand_total
+  if current_planet not in solar_system:
+    return
+  orbit_total += 1
+  for planet in solar_system[current_planet]:
+    grand_total += orbit_total
+    count_orbits(solar_system, planet, orbit_total)
+
 
 with open('input.txt') as fp:
   total_fuel = 0
-  solarSystem = defaultdict(set)
-  line = fp.readline()
-  com = line[0:3]
-  currentPlanet = line[4]
-  solarSystem[com].add(currentPlanet)
+  solar_system = defaultdict(set)
   for line in fp:
-    base = line[0]
-    currentPlanet = line[2]
-    solarSystem[base].add(currentPlanet)
-
-  totalOrbits = 0
-  currentPlanet = "COM"
-  while True:
-    for planet in solarSystem[currentPlanet]:
+    planets = line.split(")")
+    base = planets[0].rstrip()
+    current_planet = planets[1].rstrip()
+    solar_system[base].add(current_planet)
+  
+  count_orbits(solar_system, 'COM', orbit_total = 0)
+  print('Grand total: ' + str(grand_total))
 
     
